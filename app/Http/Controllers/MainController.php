450d1fe3718e51bf5c239de70027b29133010ede
keiichi kato <momonomono@gmail.com>
2025-06-15 19:45:24 +0900

    画面を作りました

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,6 +8,7 @@ class MainController extends Controller
 {
     public function todo(){
 
+        
         return view("todo.index");
     }
 }
--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -2,11 +2,30 @@
 
 @section("content")
     <div class="l-main">
-        <article>
-            <section>
-                
-            </section>
-            <section class="c-grid__content">
+        <article class="p-grid__todo">
+            <h1>Todo List</h1>
+            <ul class="p-grid__todo-list">
+                <li class="p-grid__todo-item">
+                    <div class="p-grid__todo-icon">
+                        <p class="c-text__normal">勉強をする</p>
+                    </div>
+
+                    <div class="p-grid__todo-icon">
+                        <select class="c-form__select">
+                            <option value="0">未着手</option>
+                            <option value="1">進行中</option>
+                            <option value="2">完了</option>
+                        </select>
+                        <a>
+                            <img class="c-image__icon" src="{{ asset('img/edit.png') }}">
+                        </a>
+                        <a>
+                            <img class="c-image__icon" src="{{ asset('img/delete.png') }}">
+                        </a>
+                    </div>
+                </li>
+            </ul>
+            <section class="p-grid__todo-input">
                 <input type="text" class="c-form__main">
                 <button class="c-button__main">送信</button>
             </section>
